40af5ff9f389fc491e226f5f10860416ace1acdb
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-04 21:35:25 -0700

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,11 @@
-import data from './data';
 
 function List() {
+    const data = [
+        {name: "entry", description: "entance", width:5, height:5},
+        {name: "second", description: "next", width:6, height:6},
+        {name: "third", description: "after that", width:7, height:7}
+    ];
+
     return(
         <>
             <h2>Characters</h2>
--- src/components/data.js
+++
@@ -1,5 +0,0 @@
-data = [
-    {name: "entry", description: "entance", width:5, height:5},
-    {name: "second", description: "next", width:6, height:6},
-    {name: "third", description: "after that", width:7, height:7}
-]
