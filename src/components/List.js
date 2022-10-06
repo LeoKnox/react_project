bf8e194b07caf9e25e8c06e84bed5a0367f503c9
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-05 22:18:35 -0700

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -29,7 +29,7 @@ function List() {
     }
     
     const createRoom = (e) => {
-        setRooms([...rooms, ({name:{name}, description:{description}, width:{width}, height:{height}})])
+        rooms.map({name:{name}, description:{description}, width:{width}, height:{height}})
     }
 
     return(
