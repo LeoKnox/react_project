b9a99ae24f1087f85efb536497a2dc539472de63
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-07 22:27:53 -0700

    Add files via upload

--- src/components/List.js
+++ src/components/List.js
@@ -29,9 +29,9 @@ function List() {
         setHeight(e.target.value);
     }
 
-    const deleteRoom = (deleteIndex, e) => {
+    const deleteRoom = (deleteIndex) => (e) => {
         console.log(deleteIndex)
-        console.log("***"+e);
+        e.preventDefault();
     }
     
     const createRoom = (e) => {
@@ -67,7 +67,7 @@ function List() {
                 <td><input type="text" value={description} onChange={handleDescription} /></td>
                 <td><input type="number" value={width} onChange={handleWidth} /></td>
                 <td><input type="number" value={height} onChange={handleHeight} /></td>
-                <td><button onClick={createRoom}>Create</button></td>
+                <td><button onClick={createRoom("index")}>Create</button></td>
             </tr>
             </table>
         </>
