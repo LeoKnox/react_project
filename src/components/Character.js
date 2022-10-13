5b51fb0afc51c840dc4417f34ecd16904f3c312b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-12 22:58:15 -0700

    Add files via upload

--- src/components/Character.js
+++ src/components/Character.js
@@ -17,10 +17,12 @@ function Character() {
         Math.floor(Math.random()*8),
         Math.floor(Math.random()*8)
     ]);
+    const [ans, setAns] = useState(data[Math.floor(Math.random()*8)]);
 
     function changeNumbers() {
         let x = Array(4).fill().map(() => Math.floor(Math.random()*8));
         setNumbers(x);
+        setAns(x[Math.floor(Math.random()*8)]);
     }
 
     return(
@@ -31,6 +33,7 @@ function Character() {
                 :{data[number].answer}</p>
                 </>
             ))}
+            <h3>Answer: {ans}</h3>
             <button onClick={changeNumbers}>change</button>
         </>
     )
