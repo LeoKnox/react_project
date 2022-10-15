62a018ec7bb0e5db60d7cbd179196655da471ff2
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-14 22:35:17 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import ReactDOM from 'react-dom/client';
-import List from './components/List';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Game from './game.js';
+import Index from './components/index.js';
+import Review from './components/review.js';
+import About from './about.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Create some projects</h3>
-        <List />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Game meters="home" />} />
+        <Route path="/home/" element={<Game meters="home" />} />
+        <Route path="/quiz/:id/" element={<Game grades="1" meters="quiz" />} />
+        <Route path="/quiz/" element={<Game meters="home" error="noGrades" />} />
+        <Route path="/about/" element={<Game meters="about" />} />
+        <Route path="/index/" element={<Index />}>
+          <Route path="review" element={<Review />} />
+        </Route> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/review.js
@@ -0,0 +1,7 @@
+const review = () => {
+    return (
+        <h3>now review</h3>
+    )
+}
+
+export default review;
