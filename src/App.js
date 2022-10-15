674ccc40676dda8468bb2755bef30d5e99710d0b
LeoKnox <LeoKnox@users.noreply.github.com>
2022-10-14 22:39:14 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
+import ReactDOM from 'react-dom/client';
+import Character from './components/Character';
+import List from './components/List';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Game from './game.js';
-import Index from './components/index.js';
-import Review from './components/review.js';
-import About from './about.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Game meters="home" />} />
-        <Route path="/home/" element={<Game meters="home" />} />
-        <Route path="/quiz/:id/" element={<Game grades="1" meters="quiz" />} />
-        <Route path="/quiz/" element={<Game meters="home" error="noGrades" />} />
-        <Route path="/about/" element={<Game meters="about" />} />
-        <Route path="/index/" element={<Index />}>
-          <Route path="review" element={<Review />} />
-        </Route> 
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <h3>Create some projects</h3>
+        <Character />
+        <List />
+      </header>
+    </div>
   );
 }
 
